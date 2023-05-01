9f9657f344e5328f50ac76ac9a10587a56957f42
giuliabriao <briaogiulia@gmail.com>
2023-04-30 23:22:02 -0300

    correção do path

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const path = require('path');
 
 const app = express();
 
-const pagesPath = 'dist/src/pages';
+const pagesPath = 'dist/pages';
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, pagesPath, 'index.html'));
+    res.sendFile(path.join(__dirname, pagesPath, '/index.html'));
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, pagesPath, 'login.html'));
+    res.sendFile(path.join(__dirname, pagesPath, '/login.html'));
 });
 
 app.listen(3000, () => {
